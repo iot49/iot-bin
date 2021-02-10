1921eb8c7af9e891aae12b9cc0516336945f1d59
Bernhard Boser <boser@berkeley.edu>
2021-02-09 17:51:50 -0800

    new versions

--- iotpython.sh
+++ iotpython.sh
@@ -8,11 +8,12 @@ git checkout iot
 function make_board () {
     echo ports/${1:?Port folder is required}: make BOARD=${2:?Board name is required}
     cd $IOT/iotpython/ports/$1
+    make BOARD=$2 clean
     make BOARD=$2
     cp build-$2/firmware.uf2 $IOT/iot-bin/iotpython/$2.uf2
 }
 
-make_board "nrf"         "particle_argon"
-make_board "nrf"         "feather_nrf52840_express"
-make_board "stm"         "feather_stm32f405_express"
 make_board "atmel-samd"  "feather_m4_express"
+# make_board "nrf"         "particle_argon"
+# make_board "nrf"         "feather_nrf52840_express"
+# make_board "stm"         "feather_stm32f405_express"
--- micropython.sh
+++ micropython.sh
@@ -3,21 +3,28 @@
 DATE=$(date +'20%y-%m-%d')
 
 cd $IOT/micropython
-git checkout iot
+git checkout mp
 
 # idf 4 only!
 # source $ESPIDF/export.sh
 
 function make_board () {
-    echo ports/${1:?Port folder is required}: make BOARD=${2:?Board name is required}
+    echo ports/${1:?Port folder is required}: make BOARD=${2:?Board name is required} $3
     cd $IOT/micropython/ports/$1
-    # make clean
-    make BOARD=$2
+
+    # make BOARD=$2 clean
+    make BOARD=$2 $3
+
+    # copy binaries
     cp build-$2/firmware.bin    $IOT/iot-bin/micropython/$1-$2-firmware.bin 2>/dev/null
     cp build-$2/application.bin $IOT/iot-bin/micropython/$1-$2-application.bin 2>/dev/null
     sha256sum -b build-$2/application.bin >$IOT/iot-bin/micropython/$1-$2-application.sha256 2>/dev/null
     cp build-$2/firmware.dfu    $IOT/iot-bin/micropython/$1-$2-firmware.dfu 2>/dev/null
+    cp build-$2/firmware.uf2    $IOT/iot-bin/micropython/$1-$2-firmware.uf2 2>/dev/null
 }
 
 make_board "esp32" "GENERIC_OTA"
 make_board "stm32" "ADAFRUIT_F405_EXPRESS"
+
+# make_board "nrf"   "particle_xenon" "SD=s140"
+# make_board "nrf"   "particle_argon" "SD=s140"
